32428c6ce650ce941512007fe10a5dfdc889cd8e
Gareth Dunstone <freedom.2.the.leetle.people@gmail.com>
2018-03-05 09:06:22 +1100

    extra from parameter

--- run_pipeline.sh
+++ run_pipeline.sh
@@ -5,4 +5,7 @@ TMP="/short/xe2/gdd801/tmp/$WORKING"
 
 ./tsselect/./tsselect -source "$1" -start {{start}} -end {{end}} | \
  ./tsalign/./tsalign -output "$TMP" | \
- ./tsorganize/./tsorganize -del -output "$TMP"
+ ./tsorganize/./tsorganize -del -output "$TMP" | \
+ ./tsrename/./tsrename -del -name "{{name}}~fullres" | \
+ ./tsresize/./tsresize -res 1920x1080 | \
+ ./tsrename/./tsrename -del -name "{{name}}~1920"
